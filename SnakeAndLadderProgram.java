3c63624732c823be5cf2c7d51d1e5be6c23d5f28
prathamesh <prathmesh6464@gmail.com>
2020-02-28 18:59:27 +0530

    MODIFIED : Added roll die feature.

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -1,3 +1,4 @@
+import java.util.Random; 
 public class SnakeAndLadderProgram
 {
 	public static void main(String[] args) 
@@ -8,5 +9,8 @@ public class SnakeAndLadderProgram
 		
 		//PRINTED VARIABLE NAME AND VALUE
 		System.out.println("Position : "+position);
+		Random randomValue = new Random();
+		int turnResult = randomValue.nextInt(6)+1;
+		System.out.println("Turn Result : "+turnResult);
 	}
 }
