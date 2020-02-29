b8a4b355616bac4be1ccc0e074ea5983ee49f69c
prathamesh <prathmesh6464@gmail.com>
2020-02-29 12:59:03 +0530

    MODIFIED : Added show number of times dies rolled count feature .

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -4,6 +4,7 @@ public class SnakeAndLadderProgram
 	public static void main(String[] args) 
 	{
 		//VARIABLE
+		int diceRolledCount = 0;
 		int position = 0;
 
 
@@ -12,55 +13,87 @@ public class SnakeAndLadderProgram
 			//GENERATING RANDOM NUMBERS
 			Random randomValue = new Random();
 			int valueForPlay = randomValue.nextInt(3);
-			//Random randomValue2 = new Random();
 			int turnResult = randomValue.nextInt(6)+1;
-
-
-			//SWITCH CASE FOR RUNNING OPTIONS OF GAME
+			diceRolledCount += 1;
+			
+			
+			//ELSE-IF FOR RUNNING OPTIONS OF GAME
 			if(valueForPlay == 0)
 			{
 				System.out.println("No Play");
+				
+				
+				//COUNTING NUMBER OF DICE ROLLED
+				System.out.println("Number OF Times Dice Rolled : "+diceRolledCount+" And Current Position After Dice Rolled"+position);
 			}
+
+
 			else if(valueForPlay == 1)
 			{
 				position += turnResult;
-				System.out.println("Dies Value : "+position+" Current Position After Ladder :"+position);
+				
+				
+				//COUNTING NUMBER OF DICE ROLLED
+				System.out.println("Number OF Times Dice Rolled : "+diceRolledCount+" And Current Position After Dice Rolled"+position);
+						
+				
 				if(position > 100)
 				{
+					//GIVING POSITION LAST POSITION IF VALUE IS GREATER THAN 100
 					position -= turnResult; 
-					System.out.println("Dies Value : "+position+" Current Position After Snake : "+position);
 				}
+
+
 				else if(position == 100) 
 				{
+					//PRINTING WINING MESSAGE
 					System.out.println("Player Won The Game");
 					break;
 				}
+
+
 				else if(position <= 0)
 				{
+					//GAME STARTING AGAIN
 					System.out.println("Game Started Again");
-					position=0;
+					position = 0;
 				}
 			}
+
+
 			else if(valueForPlay == 2)
 			{
 				position -= turnResult;
-				System.out.println("Dies Value : "+position+" Current Position After Snake : "+position);
+				
+				
+				//COUNTING NUMBER OF DICE ROLLED
+				System.out.println("Number OF Times Dice Rolled : "+diceRolledCount+" And Current Position After Dice Rolled"+position);
+				
+				
 				if(position > 100)
 				{
+					//GIVING POSITION LAST POSITION IF VALUE IS GREATER THAN 100
 					position -= turnResult; 
-					System.out.println("Dies Value : "+position+" Current Position After Snake : "+position);
 				}
+
+
 				else if(position == 100) 
-				{
+				{	
+					//PRINTING WINING MESSAGE
 					System.out.println("Player Won The Game");
 					break;
 				}
+
+
 				else if(position <= 0)
 				{
+					//GAME STARTING AGAIN
 					System.out.println("Game Started Again");
-					position=0;
-				}
-			}
-		}
+					position = 0;
+				}//INNER ELSE-IF ENDED
+				
+				
+			}//OUTER ELSE-IF ENDED			
+		}//WHILE LOOP ENDED
 	}	
 }
