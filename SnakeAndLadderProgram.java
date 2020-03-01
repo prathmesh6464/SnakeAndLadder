4ec22435a88137cae10ac3742e19e79682ea577b
Prathamaesh <prathmesh6464@gmail.com>
2020-03-01 14:43:31 +0530

    MODIFIED : Added nano time code.

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -1,14 +1,12 @@
 import java.util.Random; 
 public class SnakeAndLadderProgram
 {
-	static void play()
+	static boolean play()
 	{
 		//VARIABLE OF PLAYER ONE
 		int position = 0;
-		//VARIABLE OF PLAYER TWO 
-		int position2 = 0;
 
-
+		
 		while(true) 
 		{
 			//GENERATING RANDOM NUMBERS
@@ -20,97 +18,67 @@ public class SnakeAndLadderProgram
 			//ELSE-IF FOR RUNNING OPTIONS OF GAME
 			if(valueForOption == 0)
 			{
-				System.out.println("Player-1 No Play");
+				System.out.println("Player No Play");
 			}
 			else if(valueForOption == 1)
 			{
 				position += turnResult;		
-				System.out.println("Player-1 Postion : "+position);
+				System.out.println("Player Postion : "+position);
 				if(position > 100)
 				{
-					//GIVING POSITION LAST POSITION IF VALUE IS GREATER THAN 100
+					//ADDING LAST POSITION IF VALUE IS GREATER THAN 100
 					position -= turnResult; 
-					System.out.println("Player-1 Postion : "+position);
+					System.out.println("Player Postion : "+position);
 				}
 				else if(position == 100) 
 				{
-					//PRINTING WINING MESSAGE
-					System.out.println("Player-1 Won The Game");
-					break;
+					return true;
 				}				
 			}
 			else if(valueForOption == 2)
 			{
 				position -= turnResult;				
-				System.out.println("Player-1 Postion : "+position);
+				System.out.println("Postion : "+position);
 				if(position == 100) 
-				{	
-					//PRINTING WINING MESSAGE
-					System.out.println("Player-1 Won The Game");
-					break;
+				{
+					return true;
 				}
 				else if(position <= 0)
 				{
 					//GAME STARTING AGAIN
-					System.out.println("Player-1 Game Started Again");
+					System.out.println("Player Game Started Again");
 					position = 0;
-					System.out.println("Player-1 Postion : "+position);
+					System.out.println("Player Postion : "+position);
 				}//INNER ELSE-IF ENDED					
-			}//OUTER ELSE-IF ENDED		
-						
-			
-			//PLAYER TWO RANDOM NUMBER
-			int valueForOption2 = randomValue.nextInt(3);
-			int turnResult2 = randomValue.nextInt(6)+1;
-			
-						
-			//ELSE-IF FOR RUNNING OPTIONS OF GAME
-			if(valueForOption2 == 0)
-			{
-				System.out.println("Player-2 No Play");
-			}
-			else if(valueForOption2 == 1)
-			{
-				position2 += turnResult2;					
-				System.out.println("Player-2 Postion : "+position2);
-				if(position2 > 100)
-				{
-					//GIVING POSITION LAST POSITION IF VALUE IS GREATER THAN 100
-					position2 -= turnResult2; 
-					System.out.println("Player-2 Postion : "+position2);
-				}
-				else if(position2 == 100) 
-				{
-					//PRINTING WINING MESSAGE
-					System.out.println("Player-2 Won The Game");
-					break;
-				}
-			}
-			else if(valueForOption2 == 2)
-			{
-				position2 -= turnResult2;				
-				System.out.println("Player-2 Postion : "+position2);
-				if(position2 == 100) 
-				{	
-					//PRINTING WINING MESSAGE
-					System.out.println("Player-2 Won The Game");
-					break;
-				}
-				else if(position2 <= 0)
-				{
-					//GAME STARTING AGAIN
-					System.out.println("player-2 Game Started Again");
-					position2 = 0;
-					System.out.println("Player-2 Postion : "+position2);
-				}//INNER ELSE-IF ENDED				
-			}//OUTER ELSE-IF ENDED				
+			}//OUTER ELSE-IF ENDED			
 		}//WHILE LOOP ENDED
 	}
-	
-	
+
+
+	//JAVA MAIN FUNCTION
 	public static void main(String[] args) 
 	{
-		//FUNCTION CALLED
+		//FUNCTION CALLED FOR FIRST PLAYER
+		long startTimePlayer1 = System.nanoTime();
 		play();
+		long endTimePlayer1 = System.nanoTime();
+		long durationPlayer1 = (endTimePlayer1 - startTimePlayer1);
+		
+		
+		//FUNCTION CALLED FOR SECOND PLAYER
+		long startTimePlayer2 = System.nanoTime();
+		play();
+		long endTimePlayer2 = System.nanoTime();
+		long durationPlayer2 = (endTimePlayer2 - startTimePlayer2);
+		
+		
+		if(durationPlayer2 > durationPlayer1)
+		{
+			System.out.println("Player One Won The Game");
+		}
+		else
+		{
+			System.out.println("Player Two Won The Game");
+		}
 	}	
 }
